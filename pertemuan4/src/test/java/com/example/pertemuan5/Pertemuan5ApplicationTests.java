923181189013bacb59e0075555fb1e18a14a8d6f
Alif rendy <alifrendy663@gmail.com>
2025-10-22 15:34:52 +0700

    Adding pertemuan 4

---
+++ pertemuan4/src/test/java/com/example/pertemuan5/Pertemuan5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pertemuan5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pertemuan5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
